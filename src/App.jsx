a6cef5aa614d499c52f96e6881dc6a0631f2686f
Khai_Tran <tranmaiquangkhai@gmail.com>
2025-05-31 14:55:37 +0700

    feat: add routes and init structure.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import './App.css'
+import { BrowserRouter } from "react-router";
+import "./App.css";
+import { Suspense } from "react";
+import AppRoutes from "@pages/AppRoutes";
 
 function App() {
-
-
   return (
-    <>
-      
-    </>
-  )
+    <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+        <AppRoutes />
+      </Suspense>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
