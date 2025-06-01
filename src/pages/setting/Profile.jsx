c05b4b6032859ca538e0f6705e65f065ab9eadc2
realjeremy04 <tuankietvt04@gmail.com>
2025-06-01 22:51:35 +0700

    feat: setting ui #2

--- src/pages/setting/Profile.jsx
+++ src/pages/setting/Profile.jsx
@@ -662,7 +662,7 @@ export default function SettingsPage() {
       <div
         className={`
           group/sidebar fixed inset-y-0 left-0 z-50 flex w-64 flex-col bg-white shadow-lg 
-          transform transition-transform duration-300 ease-in-out lg:translate-x-0 lg:static lg:inset-0
+          transform transition-transform duration-300 ease-in-out lg:translate-x-0
           ${sidebarOpen ? "translate-x-0" : "-translate-x-full"}
         `}
       >
